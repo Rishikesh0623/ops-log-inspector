7f8619c4996d356ff7ee511b77b048f1ec21e417
Rishikesh0623 <rishikeshpulusuganti6698@gmail.com>
2026-02-10 16:37:57 +0530

    Add file upload feature and analyze log errors from uploaded file

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, render_template
 import os
 from dotenv import load_dotenv
 load_dotenv()
-LOG_FILE = os.getenv('LOG_FILE', 'simplelogs.log')
+LOG_FILE = os.getenv('LOG_FILE', 'simplelog.log')
 app = Flask(__name__)
 
 @app.route('/')
@@ -26,6 +26,22 @@ def geterror():
                 errors.append(line.strip())
 
     return jsonify({'count': len(errors), 'errors': errors})
+
+@app.route('/upload')
+def upload_page():
+    return render_template('upload.html')
+
+@app.route('/analyze', methods=['POST'])
+def analyze_log():
+    file = request.files['logfile']
+    errors = []
+
+    for line in file:
+        decoded_line = line.decode("utf-8")
+        if 'error' in decoded_line.lower():
+            errors.append(decoded_line.strip())
+    return jsonify({'count': len(errors), 'errors': errors})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
